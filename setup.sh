3f9c6dad4167e5df44334cfb623cda87a8bd589d
SilentAlice <yumingwu233@gmail.com>
2026-03-22 23:32:32 +0800

    setup: Add setup.sh for vibe branch
    Signed-off-by: SilentAlice <yumingwu233@gmail.com>

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p ~/.claude
+cp ./claude/settings.json ~/.claude/
+
+mkdir -p ~/.claude-code-router
+cp ./claude-code-router/config.json ~/.claude-code-router/
+
+mkdir -p ~/.kimi
+cp ./kimi/config.toml ~/.kimi/
